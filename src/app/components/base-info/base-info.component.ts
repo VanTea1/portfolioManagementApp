d53b2ad19fe075b62ce7c74f78e2b51c4e6cb746
VanTea1 <vanessa.kroenauer@gmail.com>
2024-04-22 11:28:09 +0200

    Added components

---
+++ src/app/components/base-info/base-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './base-info.component.html',
+  styleUrl: './base-info.component.scss'
+})
+export class BaseInfoComponent {
+
+}
