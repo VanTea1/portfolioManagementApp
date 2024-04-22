f02154d955359dea5c626220918ea8a8fce5cf52
VanTea1 <vanessa.kroenauer@gmail.com>
2024-04-22 12:28:24 +0200

    Homepage created and designed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CVComponent } from './components/cv/cv.component';
+import { ExportComponent } from './components/export/export.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'cv', component: CVComponent },
+    { path: 'export', component: ExportComponent },
+];
