c140d6b8dd88659d87f0cbe3406279568c75b0d1
VanTea1 <vanessa.kroenauer@gmail.com>
2024-04-23 13:49:36 +0200

    Added layout fillers to give a proper example on how the website is supposed to look

--- src/app/components/layouts/layouts.component.ts
+++ src/app/components/layouts/layouts.component.ts
@@ -8,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './layouts.component.scss'
 })
 export class LayoutsComponent {
+  selectedLayout: string | null = null;
 
+  //currently only changing the color of the picked layout and saving it in a variable
+  
+  toggleLayout(layoutId: string) {
+      if (this.selectedLayout === layoutId) {
+          this.selectedLayout = null;
+      } else {
+          this.selectedLayout = layoutId;
+      }
+  }
+
+  isLayoutSelected(layoutId: string): boolean {
+    return this.selectedLayout === layoutId;
+}
 }
