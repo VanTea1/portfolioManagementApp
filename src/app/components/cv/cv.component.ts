8fb6d98d52492bf7bd3d35ab7f321e608b9a45e1
VanTea1 <vanessa.kroenauer@gmail.com>
2024-05-10 13:42:06 +0200

    Added page break function, not fully implemented yet

--- src/app/components/cv/cv.component.ts
+++ src/app/components/cv/cv.component.ts
@@ -1,6 +1,4 @@
-import { Component, ElementRef, ViewChild, AfterViewInit, Input, Output, EventEmitter } from '@angular/core';
-import * as jspdf from 'jspdf';
-import html2canvas from 'html2canvas';
+import { Component, ElementRef, ViewChild, AfterViewInit, Output, EventEmitter } from '@angular/core';
 import { BaseInfoComponent } from "../base-info/base-info.component";
 import { ProjectsComponent } from "../projects/projects.component";
 import { SkillsComponent } from "../skills/skills.component";
@@ -18,17 +16,44 @@ export class CVComponent implements AfterViewInit {
     @ViewChild('exportContent', { static: false }) exportContent!: ElementRef<any>;
     @Output() exportCV = new EventEmitter<void>();
 
-    constructor() { }
+    constructor(private elementRef: ElementRef) { }
 
     ngAfterViewInit() {
+      const exportContentElement: HTMLElement = this.elementRef.nativeElement.querySelector('.cv');
         if (!this.exportContent) {
             console.error('exportContent not found.');
             return;
         }
+
+        this.insertPageBreaks(exportContentElement);
     }
 
     onExportCV() {
       this.exportCV.emit();
     }
+
+
+    //does not work yet
+    insertPageBreaks(exportContentElement: HTMLElement) {
+      const sections: NodeListOf<HTMLElement> = exportContentElement.querySelectorAll('.section');
+      const pageHeight: number = 800; 
+  
+      let currentPosition: number = 0;
+      console.log('Number of sections:', sections.length);
+      sections.forEach((section: HTMLElement) => {
+        const sectionHeight: number = section.offsetHeight;
+  
+        if (currentPosition + sectionHeight > pageHeight) {
+          const pageBreak: HTMLDivElement = document.createElement('div');
+          pageBreak.className = 'page-break';
+          exportContentElement.appendChild(pageBreak);
+  
+          currentPosition = 0; 
+        }
+  
+        currentPosition += sectionHeight;
+      });
+    }
+   
 
 }
