0f42604a8d5faacfac90efe3d565b6a481e3eb2b
VanTea1 <vanessa.kroenauer@gmail.com>
2024-05-16 11:45:06 +0200

    Worked on CSS and added a few more things to projects, for an example project

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -11,16 +11,14 @@ import { IProject } from '../../models/Project';
 })
 export class ProjectsComponent {
 
-  projects: IProject[] = [{id:1, title:'type title',description:'type description', teamsize: '1', role: 'developer', tasks: 'coding', environment: "VS Code",
+  projects: IProject[] = [{id:1, title:'Project Medical',description:'This project can....', industry:'Medical field', timeperiod:'3 months', teamsize: '1', role: 'developer', tasks: 'coding', environment: "VS Code",
     framework:"Angular", buildtools:"npm", language:"TypeScript", technical:"Win10"
    }];
 
-  projectToAdd: IProject = { id: Math.floor(Math.random() * 1000), title: 'type title', description: 'type description', teamsize: '1', role: 'developer', tasks: 'coding', environment: "VS Code",
+  projectToAdd: IProject = { id: Math.floor(Math.random() * 1000), title: 'Project Medical', description: 'This project can....', industry:'Medical field', timeperiod:'3 months', teamsize: '1', role: 'developer', tasks: 'coding', environment: "VS Code",
   framework:"Angular", buildtools:"npm", language:"TypeScript", technical:"Win10"}
 
-  //when the newly added array items get added, they also get added as a single letter item which can be deleted, it looks weird?
-  //seems to clash with how Ruslan created it, need to inquire how he did it and if he can fix it
-  //my items get added as the title and desc but also as new fields (only using the first letter for that though)
+
   
     addProject() {
     this.projects.push(this.projectToAdd);
@@ -29,7 +27,7 @@ export class ProjectsComponent {
     this.projects.splice(index, 1);
   }
   getProjectTitles(project: IProject): string[] {
-    return Object.keys(project).filter(key => key !== 'id' && key !== 'title' && key !== 'description' && key !== 'teamsize' && key !== 'role' && key !== 'framework' && key !== 'buildtools' && key !== 'language' && key !== 'technical' && key !== 'tasks' && key !== 'environment');
+    return Object.keys(project).filter(key => key !== 'id' && key !== 'title' && key !== 'description' && key !== 'industry' && key !== 'timeperiod' && key !== 'teamsize' && key !== 'role' && key !== 'framework' && key !== 'buildtools' && key !== 'language' && key !== 'technical' && key !== 'tasks' && key !== 'environment');
   }
   addProjectField(project:IProject) {
     const key = Math.floor(Math.random() * 1000).toString();
--- src/app/models/Project.ts
+++ src/app/models/Project.ts
@@ -2,6 +2,8 @@ export interface IProject {
     id: number;
     title: string;
     description: string;
+    industry: string;
+    timeperiod: string;
     teamsize: string;
     role: string;
     tasks: string;
